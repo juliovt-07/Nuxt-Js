d33983e8fa1cfc0103cd0cb968a2b3331183c3a1
Júlio César <juliocesaar.ma@gmail.com>
2020-11-20 19:31:00 -0300

    Alterações do original

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <body>
+    <div class="container">
+      <div>
+        <Logo />
+        <h1 class="title">
+          first-nuxt-project
+        </h1>
+        <div class="links">
+          <NuxtLink
+            to="/index2"
+            rel="noopener noreferrer"
+            class="button--green"
+          >
+            Dark
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.dark{
+  background: #35495e;
+}
+.dark .title, .dark .subtitle{
+  color: #fff;
+}
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/index2.vue
@@ -0,0 +1,25 @@
+<template>
+  <body class="dark">
+    <div class="container">
+      <div>
+        <LogoDark />
+        <h1 class="title">
+          first-nuxt-project
+        </h1>
+        <div class="links">
+          <NuxtLink
+            to="/"
+            rel="noopener noreferrer"
+            class="button--grey"
+          >
+            Light
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
+
+<script>
+export default {}
+</script>
